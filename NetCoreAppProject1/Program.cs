8d88f4a6d3de94ace6fb739cfb85d5a502536c5e
Ankur Joshi <Ankur.Joshi@euromonitor.com>
2018-11-23 12:54:20 +0530

    Add project files.

---
+++ NetCoreAppProject1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreAppProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
